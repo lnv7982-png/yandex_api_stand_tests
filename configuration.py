1991d9bc17c7feedb415d978665d2f6fd7b17bf0
lnv7982-png <lnv7982@gmail.com>
2026-09-10 15:06:08 +0000

    user_model.csv

--- configuration.py
+++ configuration.py
@@ -6,3 +6,5 @@ URL_SERVICE = " https://06cb8c01-b0c8-4f75-b112-ec0c01b03d34.serverhub.praktikum
 # DOC_PATH содержит путь к документации веб-сервиса
 DOC_PATH = "/docs/"
 LOG_MAIN_PATH = "/api/logs/main/"
+# Путь к таблице пользователей
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,27 +2,10 @@ import requests
 import configuration
 
 
-def get_logs():
-    """
-    Функция отправляет GET-запрос к серверу для получения логов.
-    Возвращает ответ от сервера.
-    """
-    # Формируем полный URL, соединяя базовый URL и путь к логам
-    url = configuration.URL_SERVICE + configuration.LOG_MAIN_PATH
-    
-    # Отправляем GET-запрос
-    response = requests.get(url, params={"count": 20})
-    
-    # Возвращаем ответ
-    return response
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
 
 
-# Проверяем, что всё работает
 if __name__ == "__main__":
-    response = get_logs()
-    
-    # Выводим статус-код ответа
-    print(f"Статус-код ответа: {response.status_code}")
-    
-    # Выводим заголовки ответа
-    print(f"Заголовки ответа: {response.headers}")
+    response = get_users_table()
+    print(response.status_code)
