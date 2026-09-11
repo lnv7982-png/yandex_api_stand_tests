2eedd3aa04e4dca38e9f3e93292195ee3df5fa6f
lnv7982-png <lnv7982@gmail.com>
2026-09-11 10:43:58 +0000

    Тест 3: проверка ошибки (firstName из 1 символа)

--- create_user_test.py
+++ create_user_test.py
@@ -9,36 +9,32 @@ def get_user_body(first_name):
     return current_body
 
 
-# Функция для позитивной проверки
-def positive_assert(first_name):
+# Функция для негативной проверки
+def negative_assert_symbol(first_name):
     # В переменную user_body сохраняется обновлённое тело запроса
     user_body = get_user_body(first_name)
-    # В переменную user_response сохраняется результат запроса на создание пользователя
-    user_response = sender_stand_request.post_new_user(user_body)
-    print(user_response.status_code)
 
-    # Проверяется, что код ответа равен 201
-    assert user_response.status_code == 201
-    # Проверяется, что в ответе есть поле authToken и оно не пустое
-    assert user_response.json()["authToken"] != ""
+    # В переменную response сохраняется результат запроса
+    response = sender_stand_request.post_new_user(user_body)
 
-    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
-    users_table_response = sender_stand_request.get_users_table()
+    # Проверка, что код ответа равен 400
+    assert response.status_code == 400
 
-    # Строка, которая должна быть в ответе
-    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
-               + user_body["address"] + ",,," + user_response.json()["authToken"]
-
-    # Проверка, что такой пользователь есть и он единственный
-    assert users_table_response.text.count(str_user) == 1
+    # Проверка, что в теле ответа атрибут "code" равен 400
+    assert response.json()["code"] == 400
+    
+    # Проверка текста в теле ответа в атрибуте "message"
+    assert response.json()["message"] == "Имя пользователя введено некорректно. " \
+                                         "Имя может содержать только русские или латинские буквы, " \
+                                         "длина должна быть не менее 2 и не более 15 символов"
 
 
-# Тест 2. Успешное создание пользователя
-# Параметр firstName состоит из 15 символов
-def test_create_user_15_letter_in_first_name_get_success_response():
-    positive_assert("Ааааааааааааааа")
+# Тест 3. Ошибка
+# Параметр firstName состоит из 1 символа
+def test_create_user_1_letter_in_first_name_get_error_response():
+    negative_assert_symbol("А")
 
 
 # Запускаем тест
-test_create_user_15_letter_in_first_name_get_success_response()
-print("✓ Тест 2 (15 символов) пройден успешно!")
+test_create_user_1_letter_in_first_name_get_error_response()
+print("✓ Тест 3 (1 символ) пройден успешно!")
