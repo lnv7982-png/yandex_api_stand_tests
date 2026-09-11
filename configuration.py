bdd052ef4af3b3d1512ceacf87ab629c44c88b4a
lnv7982-png <lnv7982@gmail.com>
2026-09-11 09:51:50 +0000

    Тест 2: проверка firstName из 15 символов

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 # URL_SERVICE хранит базовый URL веб-сервиса
 # ЗАМЕНИ 'https://твой-url-из-урока' на реальную ссылку из текста урока Яндекс Практикума!
 # Важно: в конце не должно быть слеша (/)
-URL_SERVICE = "https://4cbbae4f-b0b1-4495-a5e2-9c6575d769f0.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://7c1e806b-6ebd-456c-9627-aebf68b2862a.serverhub.praktikum-services.ru"
 
 # DOC_PATH содержит путь к документации веб-сервиса
 DOC_PATH = "/docs/"
--- create_user_test.py
+++ create_user_test.py
@@ -2,29 +2,43 @@ import sender_stand_request
 import data
 
 
+# Эта функция меняет значения в параметре firstName
 def get_user_body(first_name):
     current_body = data.user_body.copy()
     current_body["firstName"] = first_name
     return current_body
 
 
-# Тест 1. Успешное создание пользователя
-# Параметр firstName состоит из 2 символов
-def test_create_user_with_2_letter_first_name_gets_201_and_auth_token():
-    user_body = get_user_body("Аа")
+# Функция для позитивной проверки
+def positive_assert(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя
     user_response = sender_stand_request.post_new_user(user_body)
-    print(f"Код ответа: {user_response.status_code}")
-    print(f"Тело ответа: {user_response.text}")
+    print(user_response.status_code)
 
+    # Проверяется, что код ответа равен 201
     assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken и оно не пустое
     assert user_response.json()["authToken"] != ""
 
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
     users_table_response = sender_stand_request.get_users_table()
+
+    # Строка, которая должна быть в ответе
     str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
                + user_body["address"] + ",,," + user_response.json()["authToken"]
 
+    # Проверка, что такой пользователь есть и он единственный
     assert users_table_response.text.count(str_user) == 1
 
 
-test_create_user_with_2_letter_first_name_gets_201_and_auth_token()
-print("✓ Тест 1 пройден успешно!")
+# Тест 2. Успешное создание пользователя
+# Параметр firstName состоит из 15 символов
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Ааааааааааааааа")
+
+
+# Запускаем тест
+test_create_user_15_letter_in_first_name_get_success_response()
+print("✓ Тест 2 (15 символов) пройден успешно!")
